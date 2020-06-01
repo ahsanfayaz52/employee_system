9eebe4e81283e9675a9453c9157355e35379ade2
ahsan fayaz <ahsanfayaz52@gmail.com>
2020-06-01 15:00:25 +0500

    created handler and data packages

--- main.go
+++ main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
-
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 
 )
+type userData struct {
+	Name    string
+	Address string
+}
 
 
 func homePage(writer http.ResponseWriter,request *http.Request) {
+	switch request.Method {
+	case "GET":
+		http.ServeFile(writer, request, "form.html")
+	case "POST":
+
+		name := request.FormValue("name")
+		address := request.FormValue("address")
+		data := userData{Name: name, Address:address}
+		result,err := json.Marshal( data)
+		if (err != nil){
+			fmt.Fprintf(writer,"Error occured" ,err )
+		}
+		fmt.Fprintf(writer,string(result ))
 
 
-		fmt.Fprintf(writer,"hello world" )
+	default:
+		fmt.Fprintf(writer, "Sorry, only GET and POST methods are supported.")
 
+	}
 
 
 
